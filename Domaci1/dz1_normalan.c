cc0519533befaf88e2645e8c1fa86091a4cc274a
Amdrija <andrija.jelenkovic@protonmail.com>
2019-03-29 11:44:29 +0100

    Time Comparison

--- Domaci1/dz1_normalan.c
+++ Domaci1/dz1_normalan.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
+#define MAX_BR_EL 100000000
+void make(int *niz, int n)
+{
+    srand(time(NULL));
+    for(int i = 0; i < n; i++)
+    {   
+        niz[i] = rand() % 100000;
+    }
+}
 
-#define MAX_BR_EL 100
 
 void ucitaj(int *niz, int n)
 {
@@ -24,26 +35,46 @@ int max(int a, int b)
 }
 void vodje(int *niz, int n)
 {
-    int desni_max[n]; //i-ti element niza sadrzi maximum elemenata desno od njega
+    int maximum; //i-ti element niza sadrzi maximum elemenata desno od njega
 
-    desni_max[n - 1] = niz[n - 1];
+    maximum = niz[n - 1];
     for(int i = n - 2; i >= 0; i--)
     {
-        desni_max[i] = max(desni_max[i + 1], niz[i + 1]);
-
+        maximum = max(maximum, niz[i + 1]);
         //Ako je trenutni element veci od max svih elemenata desno od njega onda je vodja
-        if(niz[i] > desni_max[i])
+        if(niz[i] > maximum)
         {
-            printf("%d , ",niz[i]);
+            //printf("%d , ",niz[i]);
         }
     }
 
 }
+
+void neoptimizovano(int *niz, int n)
+{
+    bool jes_vodja = true;
+    for(int i = 0; i < n - 1 ; i++)
+    {
+        jes_vodja = true;
+        for(int j = i + 1; j < n; j ++)
+        {
+            if(niz[i] <= niz[j])
+            {
+                jes_vodja = false;
+                //break;
+            }
+        }
+        if(jes_vodja);
+            //printf("%d, ", niz[i]);
+    }
+}
 int main()
 {
-    int n, niz[MAX_BR_EL];
+    int n; 
 
     scanf("%d",&n);
+
+    int niz[n];
     while(n > 0)
     {
         if(n > MAX_BR_EL)
@@ -52,9 +83,15 @@ int main()
         }
         else
         {
-            ucitaj(niz,n);
+            make(niz,n);
+            time_t first = time(NULL);
             vodje(niz,n);
-            printf("\n");
+            time_t last = time(NULL);
+            printf("%lf \n",difftime(last, first));
+            first = time(NULL);
+            neoptimizovano(niz, n);
+            last = time(NULL);
+            printf("%lf \n",difftime(last, first));
         }
         scanf("%d",&n);
     }
---
+++ faktorijal.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    p = 1
+    i = 1
+    while i <= n :
+        p *= i
+        i += 1
+    
+    return p
+
+
+print(factorial(int(input())))
